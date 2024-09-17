c60bed78bcbae79cf05fea4612139f8b23e32804
NguyenHuyHoang230499 <103480136+NguyenHuyHoang230499@users.noreply.github.com>
2024-09-17 10:44:45 +0700

    Update campaigns
    - viết lại campaignController theo thiết kế
    - Sử dụng Dto cho campaign
    - Viết thêm chức năng paging cho campaign list

--- BE/MockNet04G2/MockNet04G2.Core/Common/Messages/ErrorMessages.cs
+++ BE/MockNet04G2/MockNet04G2.Core/Common/Messages/ErrorMessages.cs
@@ -13,5 +13,6 @@ namespace MockNet04G2.Core.Common.Messages
         public const string PhonelAlreadyExists = "Số điện thoại đã tồn tại!";
         public const string ConfirmPasswordDoesNotMatch = "Mật khẩu xác nhận không khớp";
         public const string ChangeRoleFailed = "Tài khoản này chưa được cập nhật quyền hạn!";
+        public const string CampaginByIdNotFound = "Không tồn tại chiến dịch quyên góp với id này";
     }
 }
--- BE/MockNet04G2/MockNet04G2.Core/Repositories/Interfaces/ICampaignRepository.cs
+++ BE/MockNet04G2/MockNet04G2.Core/Repositories/Interfaces/ICampaignRepository.cs
@@ -13,5 +13,6 @@ namespace MockNet04G2.Core.Repositories.Interfaces
         Task<List<Campaign>> GetAllCampaignsAsync();
         Task<List<Campaign>> FilterCampaignsByStatusAsync(StatusEnum status);
         Task<Campaign> GetCampaignByIdAsync(int id);
+        Task<List<Campaign>> CampaignPagingAsync(int page, int pageSize);
     }
 }
