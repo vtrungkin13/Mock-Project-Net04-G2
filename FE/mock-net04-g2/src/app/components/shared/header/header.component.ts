2d0992e76ceda111144d2d28c53f1cdd3a311d91
Kien Vu <vtrungkin13@gmail.com>
2024-09-20 00:01:07 +0700

    add update user

--- FE/mock-net04-g2/src/app/components/shared/header/header.component.ts
+++ FE/mock-net04-g2/src/app/components/shared/header/header.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { RoleEnum } from '../../../models/enum/RoleEnum';
 import { AuthService } from '../../../services/auth-service/auth.service';
+import { User } from '../../../models/User';
+import { UserProfileComponent } from "../../users/user-profile/user-profile.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, UserProfileComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  @Input() userRole?: RoleEnum;
+  @Input() user?: User;
 
   constructor(private router: Router, private authService: AuthService) {}
 
