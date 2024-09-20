6bfc5522cf722a1f90eab2e766e4e4734bb6c521
Kien Vu <vtrungkin13@gmail.com>
2024-09-20 11:16:20 +0700

    update updateUserProfile

--- FE/mock-net04-g2/src/app/components/shared/header/header.component.ts
+++ FE/mock-net04-g2/src/app/components/shared/header/header.component.ts
@@ -1,8 +1,16 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth-service/auth.service';
 import { User } from '../../../models/User';
-import { UserProfileComponent } from "../../users/user-profile/user-profile.component";
+import { UserProfileComponent } from '../../users/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-header',
@@ -11,10 +19,33 @@ import { UserProfileComponent } from "../../users/user-profile/user-profile.comp
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnChanges {
   @Input() user?: User;
+  userName?: string;
+
+  @Output() onUserUpdate = new EventEmitter();
 
   constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['user']) {
+      this.setUserName();
+    }
+  }
+
+  ngOnInit(): void {
+    this.setUserName();
+  }
+
+  setUserName() {
+    if (this.user) {
+      this.userName = this.user.name;
+    }
+  }
+
+  userUpdate() {
+    this.onUserUpdate.emit();
+  }
 
   logout() {
     this.router.navigateByUrl('/login');
